1dec3e268547d191a8aed60671339044a815e6d9
Gregory Noack <greg@squishymedia.com>
2014-10-15 13:16:47 -0700

    adding week five

--- gregorynoack/makeUser.js
+++ gregorynoack/makeUser.js
@@ -1,26 +1,51 @@
-var systemLog = {};
+
 var makeUser = (function(){
+  var systemLog = {};
   var privateUser = function(name,pwd) {
     function returnName(){
       return name;
     };
+    
     var userName = {
       name:returnName, 
-      validate:function validate(str){
+      validate:function(str){
           var hiddenPW = {pass:pwd};
           if(str == pwd){return true}else{return false}
         },
-      record:function record(message){
+      record:function(message){
         if(message == 'string' || message != undefined){
-          systemLog[name] = [];
-          systemLog[name].push(systemLog[name][message]);
-          return true;
+          if(typeof systemLog[name] != "object"){
+            systemLog[name] = [];
+            systemLog[name].push(message);
+            return true;
+          }else{
+            systemLog[name].push(message);
+            return true;
+          }
+          
         }else{
           return false;
         }
       },
+      getLog:function(username){
+        var x =""
+        if(username == undefined){
+          var i = 1;
+          for(var key in systemLog){
+              key += ":" + "\n "+systemLog[key][i];
+              console.log(key);
+              i++;
+            }
+        }else{
+          for(var i = 0; i < systemLog[username].length; i++){
+            x += username+":" + "\n "+systemLog[username][i];
+            console.log(x);
+          }
+        }
+        return x;
+      },
     };
-  return userName;
+    return userName;
   userName.returnName(name);
   userName.validate(str);
   userName.record(message);
@@ -36,4 +61,11 @@ user.record('hello world');
 user1.record('hello underworld');
 user1.record('hello underworld');
 user1.record('hello adsf');
-user1.record();
+
+var userLog = makeUser().getLog('notGreg')
+//var fullLog = makeUser().getLog()
+
+
+
+
+
